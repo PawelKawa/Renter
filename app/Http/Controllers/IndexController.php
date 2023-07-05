3feb378379308e4624b43850f45d8ecb2c3c5361
Pawel Kawa <pawel.kawa@classroomsecrets.co.uk>
2023-07-05 22:59:28 +0100

    created ListingFactory to populate db with fake data

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Listing;
 
 class IndexController extends Controller
 {
     public function index(){
+        // I can see all my queries on index.vue with code below
+        // dd(Listing::all());
         return inertia('Index/index',
     [
         //go to web dev vue and index chech attribute it will be there this message
