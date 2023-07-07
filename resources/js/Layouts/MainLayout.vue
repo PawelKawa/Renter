73d37aa44bbb4257c9e7b038af630797aee078e9
Pawel Kawa <pawel.kawa@classroomsecrets.co.uk>
2023-07-07 08:33:18 +0100

    Added flash msgs

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,5 +1,7 @@
-
 <template>
+  <div v-if="page.props.flash.success" class="flash">
+    {{page.props.flash.success}}
+  </div>
   <Link class="text-red-500" href="/">
      Main page
   </Link>&nbsp;
@@ -13,8 +15,15 @@
 
 <script setup>
 import {ref} from 'vue'
-
+import { usePage } from '@inertiajs/vue3'
+const page = usePage()
 const timer = ref(0)
 setInterval(() => timer.value++, 1000)
 defineExpose({ timer })
 </script>
+
+<style scoped>
+  .flash{
+    background-color: green;
+  }
+</style>
