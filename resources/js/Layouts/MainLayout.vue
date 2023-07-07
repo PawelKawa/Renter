d96ccf7b78f3020bb84abf7608fdf20e6f503e25
Pawel Kawa <pawel.kawa@classroomsecrets.co.uk>
2023-07-07 23:29:03 +0100

    Learning computed

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
-  <div v-if="page.props.flash.success" class="flash">
-    {{page.props.flash.success}}
+  <div v-if="flashSuccess" class="flash">
+    {{flashSuccess}}
   </div>
   <Link class="text-red-500" href="/">
      Main page
@@ -9,15 +9,23 @@
     Go to Listings page
   </Link>&nbsp;
   <p>{{ timer }}</p>
-    
+    <p>{{y}}</p>
+    <button class="mr-2 border" @click="plus">add</button>
+    <button class="border" @click="x--">subtract</button>
   <slot />
 </template>
 
 <script setup>
 import {ref} from 'vue'
+import {computed} from 'vue'
 import { usePage } from '@inertiajs/vue3'
 const page = usePage()
 const timer = ref(0)
+const flashSuccess = computed( () => page.props.flash.success )
+const x = ref(0);
+const y = computed( () => x.value * 2 )
+const plus = () => x.value++
+
 setInterval(() => timer.value++, 1000)
 defineExpose({ timer })
 </script>
