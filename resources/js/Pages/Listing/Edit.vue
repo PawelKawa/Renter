b21079bfba5ff2eeb0cbe564360721dab9842321
Pawel Kawa <pawel.kawa@classroomsecrets.co.uk>
2023-07-09 21:41:10 +0100

    Styled edit form

--- resources/js/Pages/Listing/Edit.vue
+++ resources/js/Pages/Listing/Edit.vue
@@ -1,110 +1,109 @@
 <template>
-  <form @submit.prevent="update">
-    <div>
-      <div>
-        <label>Beds</label>
-        <input v-model.number="form.beds" type="text" />
-        <div v-if="form.errors.beds">
-          {{ form.errors.beds }}
-        </div>
-      </div>
-
-      <div>
-        <label>Baths</label>
-        <input v-model.number="form.baths" type="text" />
-      </div>
-      <div v-if="form.errors.baths">
-        {{ form.errors.baths }}
-      </div>
-      <div>
-        <label>Area</label>
-        <input v-model.number="form.area" type="text" />
-      </div>
-      <div v-if="form.errors.area">
-        {{ form.errors.area }}
-      </div>
-      <div>
-        <label>City</label>
-        <input v-model="form.city" type="text" />
-      </div>
-      <div v-if="form.errors.city">
-        {{ form.errors.city }}
-      </div>
-      <div>
-        <label>Post Code</label>
-        <input v-model="form.code" type="text" />
-      </div>
-      <div v-if="form.errors.code">
-        {{ form.errors.code }}
-      </div>
-      <div>
-        <label>Street</label>
-        <input v-model="form.street" type="text" />
-      </div>
-      <div v-if="form.errors.street">
-        {{ form.errors.street }}
-      </div>
-      <div>
-        <label>Street Nr</label>
-        <input v-model.number="form.street_nr" type="text" />
-      </div>
-      <div v-if="form.errors.street_nr">
-        {{ form.errors.street_nr }}
-      </div>
-      <div>
-        <label>Price</label>
-        <input v-model.number="form.price" type="text" />
-      </div>
-      <div v-if="form.errors.price">
-        {{ form.errors.price }}
-      </div>
-      <div>
-        <button type="submit">Update</button>
-      </div>
-    </div>
-  </form>
+	<form @submit.prevent="update">
+		<div class="grid grid-cols-6 gap-4">
+			<div class="col-span-2">
+				<label class="label">Beds</label>
+				<input class="input" v-model.number="form.beds" type="text" />
+				<div v-if="form.errors.beds">
+					{{ form.errors.beds }}
+				</div>
+			</div>
+			<div class="col-span-2">
+				<label class="label">Baths</label>
+				<input class="input" v-model.number="form.baths" type="text" />
+				<div v-if="form.errors.baths">
+					{{ form.errors.baths }}
+				</div>
+			</div>
+			<div class="col-span-2">
+				<label class="label">Area</label>
+				<input class="input" v-model.number="form.area" type="text" />
+				<div v-if="form.errors.area">
+					{{ form.errors.area }}
+				</div>
+			</div>
+			<div class="col-span-4">
+				<label class="label">City</label>
+				<input class="input" v-model="form.city" type="text" />
+				<div v-if="form.errors.city">
+					{{ form.errors.city }}
+				</div>
+			</div>
+			<div class="col-span-2">
+				<label class="label">Post Code</label>
+				<input class="input" v-model="form.code" type="text" />
+				<div v-if="form.errors.code">
+					{{ form.errors.code }}
+				</div>
+			</div>
+			<div class="col-span-4">
+				<label class="label">Street</label>
+				<input class="input" v-model="form.street" type="text" />
+				<div v-if="form.errors.street">
+					{{ form.errors.street }}
+				</div>
+			</div>
+			<div class="col-span-2">
+				<label class="label">Street Nr</label>
+				<input class="input" v-model.number="form.street_nr" type="text" />
+				<div v-if="form.errors.street_nr">
+					{{ form.errors.street_nr }}
+				</div>
+			</div>
+			<div class="col-span-6">
+				<label class="label">Price</label>
+				<input class="input" v-model.number="form.price" type="text" />
+				<div v-if="form.errors.price">
+					{{ form.errors.price }}
+				</div>
+			</div>
+			<div>
+				<button class="primary-btn" type="submit">Update</button>
+			</div>
+		</div>
+	</form>
 </template>
 
 <script setup>
-// import { reactive } from 'vue'
-import { useForm } from '@inertiajs/vue3'
-const props = defineProps({
-    listing: Object,
-})
-const form = useForm({
-    beds: props.listing.beds,
-    baths: props.listing.baths,
-    area: props.listing.area,
-    city: props.listing.city,
-    code: props.listing.code,
-    street: props.listing.street,
-    street_nr: props.listing.street_nr,
-    price: props.listing.price,
+	// import { reactive } from 'vue'
+	import { useForm } from '@inertiajs/vue3'
+	const props = defineProps({
+		listing: Object,
+	})
+	const form = useForm({
+		beds: props.listing.beds,
+		baths: props.listing.baths,
+		area: props.listing.area,
+		city: props.listing.city,
+		code: props.listing.code,
+		street: props.listing.street,
+		street_nr: props.listing.street_nr,
+		price: props.listing.price,
 
-})
-// const update = () => form.put(`/listing/${props.listing.id}`)
-const update = () => form.put(route('listing.update', props.listing.id))
+	})
+	// const update = () => form.put(`/listing/${props.listing.id}`)
+	const update = () => form.put(route('listing.update', props.listing.id))
 
 </script>
 
 <script>
-import MainLayout from '@/Layouts/MainLayout.vue'
-export default {
-    layout: MainLayout,
-}
+	import MainLayout from '@/Layouts/MainLayout.vue'
+	export default {
+		layout: MainLayout,
+	}
 </script>
 
 <!-- Loading something like presistent Layouts and thanks to that timer is not restarting when swaping pages. -->
 <style scoped>
-    label {
-        margin-right: 2em;
-    }
+	label {
+		margin-right: 2em;
+	}
 
-    div {
-        padding: 2px
-    }
+	div {
+		padding: 2px
+	}
 
-    input {
-        border: 1px black solid;
-    }
+	input {
+		border: 1px black solid;
+	}
 </style>
