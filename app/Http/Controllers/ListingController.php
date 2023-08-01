fcc48229027da126987a72f10baa5e6a0fe82f35
Pawel Kawa <pawel.kawa@classroomsecrets.co.uk>
2023-08-01 23:31:05 +0100

    Handle filters in backend, simple way, need to try Eloquent

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -19,13 +19,41 @@ class ListingController extends Controller
      */
     public function index(Request $request)
     {
+        $query = Listing::orderByDesc('created_at');
+
+        $filters = $request->only([
+            'priceFrom', 'priceTo', 'beds', 'baths', 'areaFrom', 'areaTo'
+        ]);
+
+        if ($filters['priceFrom'] ?? false) {
+            $query->where('price', '>=', $filters['priceFrom']);
+        }
+
+        if ($filters['priceTo'] ?? false) {
+            $query->where('price', '<=', $filters['priceTo']);
+        }
+
+        if ($filters['beds'] ?? false) {
+            $query->where('beds', $filters['beds']);
+        }
+
+        if ($filters['baths'] ?? false) {
+            $query->where('baths', $filters['baths']);
+        }
+
+        if ($filters['areaFrom'] ?? false) {
+            $query->where('area', '>=', $filters['areaFrom']);
+        }
+
+        if ($filters['areaTo'] ?? false) {
+            $query->where('area', '<=', $filters['areaTo']);
+        }
+
         return inertia(
             'Listing/Index',
             [
-                'filters' => $request->only([
-                    'priceFrom', 'priceTo', 'beds', 'baths', 'areaFrom', 'areaTo'
-                ]),
-                'listings' => Listing::orderByDesc('created_at')
+                'filters' => $filters,
+                'listings' => $query
                 ->paginate(10)
                 ->withQueryString()
             ]
