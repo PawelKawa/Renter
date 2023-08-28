d0fd9677d1f502cdc290f511dcf84fd5617a047a
Pawel Kawa <pawel.kawa@classroomsecrets.co.uk>
2023-08-28 22:14:12 +0100

    Fixed the error at pagination when expected null but received Null...

--- resources/js/Components/UI/Pagination.vue
+++ resources/js/Components/UI/Pagination.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex gap-1">
-        <Link v-for="(link, index) in links" :key="index" :href="link.url" class="py-2 px-4 rounded-md"
-            :class="{'bg-indigo-800 dark:bg-indigo-400 text-gray' :link.active}" v-html="link.label">
+        <Link v-for="(link, index) in links" :key="index" :href="link.url || '#'" class="py-2 px-4 rounded-md"
+            :class="{'bg-indigo-800 dark:bg-indigo-400 text-gray': link.active}" v-html="link.label">
         </Link>
     </div>
 </template>
